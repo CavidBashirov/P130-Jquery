a9a124396f5f0fcb3404b38c67f83416f60d79d6
Cavid Bashirov <bashirovcavid93gmail.com>
2022-09-07 17:44:16 +0400

    initial

---
+++ assets/js/script.js
@@ -0,0 +1,197 @@
+// $(document).ready(function () {
+
+// });
+
+$(function () {
+
+
+    //let elem = document.querySelector("h4").innerText;
+
+    // let elems = $("#products h4");
+
+    // for (const item of elems) {
+    //     console.log(item);
+    // }
+
+
+
+
+    // $("#products button").first().click(function(){
+    //     // console.log($(this).html(`<span class="text-danger">Bye bye</span>`));
+    //     $("#products .box").hide(2000);
+    //     $("h4").css({
+    //         "font-size":"50px",
+    //         "color":"yellow"
+    //     })
+    // })
+
+
+    // $("#products button").first().next().click(function(){
+    //     // console.log($(this).html(`<span class="text-danger">Bye bye</span>`));
+    //     $("#products .box").show(2000,function(){
+    //         $("#products .box").css("background-color","blue")
+    //     });
+    // })
+
+    // $("#products button").last().click(function(){
+    //     // console.log($(this).html(`<span class="text-danger">Bye bye</span>`));
+    //     $("#products .box").toggle(2000,function(){
+    //         $("#products .box").css("background-color","blue")
+    //     });
+    // })
+
+    // $("button").click(function(){
+    //     let newLi=$('<li class="list-group-item">An item</li>');
+    //     $("ul").append(newLi);
+    // })
+
+
+    // $("ul li").click(function(){
+    //     alert("clicked")
+    // })
+
+
+    // $(document).on("mouseup","input",function(){
+    //     alert("downed")
+    // })
+
+
+
+    // $(document).on("dblclick","ul li",function(){
+    //     alert("clicked")
+    // })
+
+
+    // $("#products .first-btn").click(function(){
+    //     $("#products .box").fadeTo("slow",0.7);
+    // })
+
+    // $("#products .second-btn").click(function(){
+    //     $("#products .box").fadeIn(2000);
+    // })
+
+
+    // $("#products .third-btn").click(function(){
+    //     $("#products .box").fadeToggle(2000);
+    // })
+
+    // $("#products .first-btn").click(function(){
+    //     $("#products .box").slideUp(2000);
+    // })
+
+    // $("#products .second-btn").click(function(){
+    //     $("#products .box").slideDown(2000,function(){
+    //         alert("welcome")
+    //     });
+    // })
+
+
+    // $("#products .third-btn").click(function(){
+    //     $("#products .box").slideUp(2000).slideDown(2000);(2000);
+    // })
+
+
+    // $("#products .first-btn").click(function () {
+    //     $("#products .box").animate({
+    //         left: '250px',
+    //         opacity: '0.5',
+    //         height: '150px',
+    //         width: '150px',
+
+    //     },2000).css("background-color","yellow");
+    // })
+
+
+    // $("#products .second-btn").click(function(){
+    //     $("#products .box").animate({
+    //         left: '0',
+    //         opacity: '1',
+    //         height: '300px',
+    //         width: '200px',
+
+    //     },2000).css("background-color","blue");
+    // })
+
+
+
+    // $(document).on("click", ".third-btn", function () {
+    //     if ($(".text-input").val() == "") {
+    //         return;
+    //     }
+    //     alert($(".text-input").val());
+
+    // })
+
+
+
+    // $("button").click(function () {
+    //     $("h4").before($('<h3>An item</h3>'));
+    // })
+
+    // $("button").click(function () {
+    //     $("ul").empty();
+    // })
+
+
+    // $(".add").click(function () {
+    //     $("ul li").toggleClass("active-elem");
+    // })
+
+    // $(".remove").click(function () {
+    //     $("ul li").removeClass("active-elem");
+    // })
+
+
+    // $(".add").click(function () {
+    //    let newSize = $(".box").width() * 3 + "px";
+    //    $(".box").css("width",newSize);
+
+    // })
+
+    // $(".add").click(function () {
+
+    //     let newWidth = $(".box").width() + "px";
+
+    //     let newHeight = $(".box").height() + "px";
+
+    //     $(".box2").css({
+    //         "width": newWidth,
+    //         "height": newHeight
+    //     })
+
+    // })
+
+
+    // console.log($(".container.my-5").children().last())
+
+
+    console.log($("*"))
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
